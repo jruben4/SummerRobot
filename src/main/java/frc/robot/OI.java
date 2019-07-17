b883c46d7aed48fcb25065bb3248463d5d7f3571
Jason <jruben4@yahoo.com>
2019-07-16 19:05:58 -0500

    init

---
+++ src/main/java/frc/robot/OI.java
@@ -0,0 +1,32 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+
+
+public class OI {
+    private GenericHID controller = new GenericHID(0){
+    
+        @Override
+        public double getY(Hand lHand) {
+            return 0;
+        }
+    
+        @Override
+        public double getX(Hand lHand) {
+            return 0;
+        }
+    };
+    
+    public boolean getButton(int button){
+        return controller.getRawButtonPressed(button);
+    }
+}
+
+
